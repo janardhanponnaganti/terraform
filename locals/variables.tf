52016a7f3f7f8e3139b3aa394b8f51af00f0d3d4
janardhanponnaganti <ponnagantijr@gmail.com>
2024-01-09 21:21:33 +0100

    teraform

---
+++ locals/variables.tf
@@ -0,0 +1,16 @@
+variable "isProd" {
+  type = bool
+  default = true
+}
+
+variable "ami_id" {
+  default = "ami-03265a0778a880afb"
+}
+
+variable "zone_id" {
+  default = "Z07194143L4ZMKYDIMA6D"
+}
+
+variable "domain_name" {
+  default = "ponnaganti.online"
+}
